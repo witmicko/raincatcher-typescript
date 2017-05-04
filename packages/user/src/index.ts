f85dc6abe9d03c0e8b1564cb386f7c58042478af
Paolo Bueno <paolohaji@gmail.com>
2017-05-04 13:04:37 -0300

    Remove events and add morgan for logging instead

--- packages/user/src/index.ts
+++ packages/user/src/index.ts
@@ -1,6 +1,5 @@
 import * as express from 'express';
 import { Store } from '@raincatcher/store';
-import { EventEmitter } from 'events';
 
 export interface User {
 	id : string;
@@ -8,33 +7,19 @@ export interface User {
   email : string;
 }
 
-/**
- * Extra interface to allow the express router to have an EventEmitter
- */
-export interface EventedRouter extends express.Router {
-  events: EventEmitter;
-}
-
 export default function initializeRouter(store: Store<User>) {
-  const router = express.Router() as EventedRouter;
-  router.events = new EventEmitter();
+  const router: express.Router = express.Router();
 
   const route = router.route('/');
 
   route.get(function(req, res) {
     store.list()
-      .tap(function(list: User[]) {
-        router.events.emit('list', list);
-      })
       .then(res.json.bind(res));
   });
 
   route.post(function(req, res) {
     var userToCreate = req.body;
     store.add(userToCreate)
-      .tap(function(newUser: User) {
-        router.events.emit('list', newUser);
-      })
       .then(res.json.bind(res));
   });
 
